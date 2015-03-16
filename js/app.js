a62a05068a8c8c8db6f9c977003c2909508025c8
ronrith <sarornrith@gmail.com>
2015-03-16 10:35:19 -0400

    Final 2

--- js/app.js
+++ js/app.js
@@ -1,16 +1,26 @@
 $(document).ready( function(){
 	$(".title").hide().html("Fizz Buzz!").fadeIn(1000);
 	$(".iStart").on("click", function(){
-		fizzStart(); 
+		var m = + $(".iNum").val()+1;
+		fizzStart(m); 
 	}); 
 
-});
+}); 
 
 // ========================
+ 
+function fizzStart(m){
+	console.log(m);
+	if (isNaN(m)){
+		alert("Please supply a number.");
+		return;
+	}
 
-function fizzStart(){
-	var m = parseInt($(".iNum").val())+1;
-	// console.log(m);
+	if (m % 1 != 0){
+		alert("No decimal.");
+		return;
+	}
+
 	for (var i=1; i < m; i++){
 		if ( i%3 == 0 && i%5 == 0 ){
 			$(".content").append(i + ". <b>Fizz Buzz!</b> <br>");
